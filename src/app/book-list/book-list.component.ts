38790e1d9dea38899999f11dca3f2cf8abec55f6
matteutsch <matteutsch@gmail.com>
2023-10-24 19:56:34 +0200

    added book-list-item

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -12,20 +12,21 @@ export class BookListComponent {
   constructor() {
     this.books = [
       {
-      isbn: '123123',
-      title: 'Tierisch gut kochen',
-      authors: ['Mrs Chimp', 'Mr Gorilla'],
-      subtitle: 'Rezepte von Affe bis Zebra',
-      thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
-      description: 'Immer lecker und gut'
-    },
-    {
-      isbn: '123123',
-      title: 'Tierisch gut kochen',
-      authors: ['Mrs Chimp', 'Mr Gorilla'],
-      subtitle: 'Rezepte von Affe bis Zebra',
-      thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
-      description: 'Immer lecker und gut'
-    }];
+        isbn: '123123',
+        title: 'Tierisch gut kochen',
+        authors: ['Mrs Chimp', 'Mr Gorilla'],
+        subtitle: 'Rezepte von Affe bis Zebra',
+        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
+        description: 'Immer lecker und gut',
+      },
+      {
+        isbn: '039384',
+        title: 'Backen mit Affen',
+        authors: ['Orang Utan'],
+        subtitle: 'Bananenbrot und mehr',
+        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
+        description: 'Tolle Backtipps für Mensch und Tier',
+      },
+    ];
   }
 }
