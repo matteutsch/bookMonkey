0bfc4942d2f77b1b0803e11a1d08efebf5ccb00d
matteutsch <matteutsch@gmail.com>
2023-11-15 18:46:04 +0100

    added errormessages

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,9 +7,10 @@ import { BookFormComponent } from './book-form/book-form.component';
 import { BookCreateComponent } from './book-create/book-create.component';
 import { LocalIsoDateValueAccessor } from 'angular-date-value-accessor';
 import { BookEditComponent } from './book-edit/book-edit.component';
+import { FormErrorsComponent } from './form-errors/form-errors.component';
 
 @NgModule({
-  declarations: [BookFormComponent, BookCreateComponent, BookEditComponent],
+  declarations: [BookFormComponent, BookCreateComponent, BookEditComponent, FormErrorsComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
