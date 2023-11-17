c43ab3bd7d15301ec53ec14302e7b97a8e212043
matteutsch <matteutsch@gmail.com>
2023-11-17 16:42:29 +0100

    added custom pipe

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -5,12 +5,14 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { IsbnPipe } from './shared/isbn.pipe';
 
 @NgModule({
   declarations: [
     BookListComponent,
     BookListItemComponent,
     BookDetailsComponent,
+    IsbnPipe,
   ],
   imports: [CommonModule, BooksRoutingModule],
 })
